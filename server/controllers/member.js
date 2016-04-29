c5125a50431550b986b8eca88da07d6818a2bdc9
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-29 23:52:10 +0300

    [#118674291] Adds Procfile

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -8,27 +8,18 @@
   module.exports = {
     // Function to create a new member
     create: function(req, res) {
-      // Check if Email exists
-      Member.findOne(req.body.email, function(err, result) {
-        if (result) {
-          return res.status(409).json({
-            message: 'This Email already exists'
-          });
-        } else {
-          // Instance of the model Member
-          var member = new Member();
-          member.name = req.body.name;
-          member.email = req.body.email;
+      // Instance of the model Member
+      var member = new Member();
+      member.name = req.body.name;
+      member.email = req.body.email;
 
-          member.save(function(err, savedMember) {
-            if (err) {
-              console.log(err);
-              return res.status(500).send(err);
-            } else {
-              return res.status(200).json({
-                message: 'Successfully added new user'
-              });
-            }
+      member.save(function(err, savedMember) {
+        if (err) {
+          console.log(err);
+          return res.status(500).send(err);
+        } else {
+          return res.status(200).json({
+            message: 'Successfully added new user'
           });
         }
       });
