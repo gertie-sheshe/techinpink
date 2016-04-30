a8a08538ce8a50b25e72b40a87c94033a463819e
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-30 05:16:32 +0300

    [#118674291] Changes database username

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@
 
   // Export it so that it is accessible to otther files
   module.exports = {
-    db: 'mongodb://sheshe:shshe@ds033103.mlab.com:33103/startfromscartch'
+    db: 'mongodb://sheshe:sheshe@ds033103.mlab.com:33103/startfromscartch'
   };
 })();
