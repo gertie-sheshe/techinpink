33f07096d5810de9345381a60879d870d9016a1f
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-04-30 05:11:56 +0300

    [#118674291] Adds Mongolab

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@
 
   // Export it so that it is accessible to otther files
   module.exports = {
-    db: 'mongodb://localhost:27017/techinpink'
+    db: 'mongodb://sheshe:shshe@ds033103.mlab.com:33103/startfromscartch'
   };
 })();
